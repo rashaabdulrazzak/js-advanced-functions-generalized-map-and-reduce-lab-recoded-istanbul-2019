1222385a1239a58eaf9e7f880d82bf3494db7ec0
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-13 07:29:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, func ){
+  let newArray =[];
+  for (let element of array)
+  {
+       let val = func(element);
+       newArray.push(val);
+  }
+  return newArray;
+}
+function reduce(array,func,start){
+   let val = 0;
+  if(typeof(start) ==='undefined'){
+    val = 0;
+  }
+  else val=start;
+  if (typeof func(true,true) == 'boolean') val =true 
+
+  for(let element of array){
+    val = func(element,val);
+    
+  }
+  return val;
+}
